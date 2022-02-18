811a5bbb40e49992a19c7a804ddc58e96d63630d
JerryAllMighty <dbsalszz@naver.com>
2022-02-18 18:29:37 +0900

    3계층 구성, 모델 User 관련 파일 생성

--- src/main/java/com/sparta/juteukki02/juteukki_week02/controller/UserController.java
+++ src/main/java/com/sparta/juteukki02/juteukki_week02/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.sparta.juteukki02.juteukki_week02.controller;
 
+import com.sparta.juteukki02.juteukki_week02.model.User;
 import com.sparta.juteukki02.juteukki_week02.model.UserRepository;
+import com.sparta.juteukki02.juteukki_week02.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +12,11 @@ import java.util.List;
 @RestController
 public class UserController {
     private final UserService userService;
-    private final UserRepository commentRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("/api/comments/{postid}")
-    public List<Comment> getPosts(@PathVariable Long postid){
-        return commentRepository.findByPostId(postid.toString());
+    public List<User> getPosts(@PathVariable Long postid){
+        return userRepository.findAll();
     }
 //    @GetMapping("/api/comments")
 //    public List<CommentsContentsOnly> getPosts(){
@@ -22,21 +24,21 @@ public class UserController {
 //    }
 
 
-    @PostMapping("/api/comments")
-    public Comment createProduct(@RequestBody CommentDto commentDto) {
-        Comment comment = new Comment(commentDto);
-        commentRepository.save(comment);
-        return comment;
-    }
-    @PutMapping("/api/comments/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody CommentDto commentDto) {
-        return commentservice.update(id, commentDto);
-    }
-
-    @DeleteMapping("/api/comments/{id}")
-    public Long deleteMemo(@PathVariable Long id) {
-        commentRepository.deleteById(id);
-        return id;
-    }
+//    @PostMapping("/api/comments")
+//    public Comment createProduct(@RequestBody CommentDto commentDto) {
+//        Comment comment = new Comment(commentDto);
+//        commentRepository.save(comment);
+//        return comment;
+//    }
+//    @PutMapping("/api/comments/{id}")
+//    public Long updateProduct(@PathVariable Long id, @RequestBody CommentDto commentDto) {
+//        return commentservice.update(id, commentDto);
+//    }
+//
+//    @DeleteMapping("/api/comments/{id}")
+//    public Long deleteMemo(@PathVariable Long id) {
+//        commentRepository.deleteById(id);
+//        return id;
+//    }
 
 }
